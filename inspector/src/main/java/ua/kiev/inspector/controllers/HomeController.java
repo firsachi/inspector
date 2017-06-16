3f6bfb01fec777f80a104de7d088eb91e0d30c6e
firsov <firsov@firs.kievreclama.local>
2017-06-16 17:48:33 +0300

    i add paramert user

--- inspector/src/main/java/ua/kiev/inspector/controllers/HomeController.java
+++ inspector/src/main/java/ua/kiev/inspector/controllers/HomeController.java
@@ -1,9 +1,12 @@
 package ua.kiev.inspector.controllers;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import ua.kiev.inspector.model.UserModel;
 
 @Controller
 public class HomeController {
@@ -12,6 +15,8 @@ public class HomeController {
 	public ModelAndView homePage(){
 		ModelAndView model = new ModelAndView();
 		model.setViewName("home");
+		UserModel user = (UserModel)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		model.addObject("user", user);
 		return model;
 	}
 
