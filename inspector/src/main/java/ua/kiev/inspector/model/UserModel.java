0c65b310b6781f409e00b76896fa8d8d601e0a7b
firsov <firsov@firs.kievreclama.local>
2017-06-16 17:49:27 +0300

    i add imterface UserDetails

--- inspector/src/main/java/ua/kiev/inspector/model/UserModel.java
+++ inspector/src/main/java/ua/kiev/inspector/model/UserModel.java
@@ -1,6 +1,11 @@
 package ua.kiev.inspector.model;
 
-public class UserModel {
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class UserModel implements UserDetails{
 	
 	private String firstName;
 	private String lastName;
@@ -51,13 +56,37 @@ public class UserModel {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
+	
+	public String getFullUserName(){
+		return firstName + " " + lastName;
+	}
 	public boolean isEnabled() {
 		return enabled;
 	}
 
 	public void setEnabled(boolean enabled) {
 		this.enabled = enabled;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return false;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return false;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return false;
 	}
 	
 	
