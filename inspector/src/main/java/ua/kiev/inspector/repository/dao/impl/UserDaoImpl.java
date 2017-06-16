c06f61f5e8b130aad42dd82918b5d1410daee7e8
firsov <firsov@firs.kievreclama.local>
2017-06-16 17:50:24 +0300

    i implement find user

--- inspector/src/main/java/ua/kiev/inspector/repository/dao/impl/UserDaoImpl.java
+++ inspector/src/main/java/ua/kiev/inspector/repository/dao/impl/UserDaoImpl.java
@@ -1,5 +1,9 @@
 package ua.kiev.inspector.repository.dao.impl;
 
+import java.util.List;
+
+import javax.persistence.TypedQuery;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +21,20 @@ public class UserDaoImpl implements UserDao{
 	@Override
 	public User findByUserName(String username) {
 		Session session = sessionFactory.getCurrentSession();
+		String query = "SELECT u FROM User u WHERE u.mail = :username";
+		TypedQuery<User> typedQuery = session.createQuery(query, User.class);
+		typedQuery.setParameter("username", username);
+		List<User> users = typedQuery.getResultList();
+		if (users.size() > 0) {
+			return users.get(0);
+		} else {
+			return null;
+		}
+	}
+
+	@Override
+	public List<User> getAll() {
+		// TODO Auto-generated method stub
 		return null;
 	}
 
