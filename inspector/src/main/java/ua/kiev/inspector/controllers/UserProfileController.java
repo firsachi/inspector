23a115ebb65e44ec6e78d2c68a641d1f61ae6a50
firsov <firsov@firs.kievreclama.local>
2017-06-16 17:48:51 +0300

    to do

--- inspector/src/main/java/ua/kiev/inspector/controllers/UserProfileController.java
+++ inspector/src/main/java/ua/kiev/inspector/controllers/UserProfileController.java
@@ -1,18 +1,28 @@
 package ua.kiev.inspector.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import ua.kiev.inspector.model.UserModel;
+import ua.kiev.inspector.services.BaseService;
+import ua.kiev.inspector.services.UserService;
 
 @Controller
 public class UserProfileController {
+	
+	@Autowired
+	private BaseService<UserModel> userService;
 
 	@RequestMapping(value = "/user-profile", method = RequestMethod.GET)
-	public ModelAndView pageUserProfile(){
+	public ModelAndView pageUserProfile(Authentication authentication){
+		UserModel user = (UserModel)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 		ModelAndView model = initModelAndView("user-profile");
+		model.addObject("user", user);
 		return model;
 	}
 	
