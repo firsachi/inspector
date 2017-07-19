af1933987789a07a18e2fd3a7484a6ae0b9af8a5
firsachi <firsachi@localhost>
2017-07-19 08:01:21 +0300

    i add driver mysql add lrazy connection factory

--- inspector/src/main/java/ua/kiev/inspector/configurations/HibernateConfiguration.java
+++ inspector/src/main/java/ua/kiev/inspector/configurations/HibernateConfiguration.java
@@ -7,6 +7,7 @@ import java.util.Properties;
 import org.hibernate.SessionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -16,18 +17,24 @@ import com.mchange.v2.c3p0.ComboPooledDataSource;
 @Configuration
 @EnableTransactionManagement
 public class HibernateConfiguration {
+	
+	@Bean
+	public LazyConnectionDataSourceProxy dataSource(){
+		LazyConnectionDataSourceProxy dataSource = new LazyConnectionDataSourceProxy(templateDataSource());
+		return dataSource;
+	}
 
 	@Bean
-	public ComboPooledDataSource dataSource(){
+	public ComboPooledDataSource templateDataSource(){
 		ComboPooledDataSource dataSource = new ComboPooledDataSource();
 		try {
-			dataSource.setDriverClass("org.postgresql.Driver");
-			dataSource.setJdbcUrl("jdbc:postgresql://localhost:5432/inspector");
-			dataSource.setUser("sainspector");
-			dataSource.setPassword("!Q2w3e$R");
-			dataSource.setMaxPoolSize(30);
+			dataSource.setDriverClass("com.mysql.jdbc.Driver");
+			dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/inspector");
+			dataSource.setUser("root");
+			dataSource.setPassword("Alex1q2w#E");
+			dataSource.setMaxPoolSize(10);
 			dataSource.setMinPoolSize(2);
-			dataSource.setMaxStatements(100);
+			dataSource.setIdleConnectionTestPeriod(3600);
 		} catch (PropertyVetoException e) {
 			e.printStackTrace();
 		}
@@ -40,8 +47,10 @@ public class HibernateConfiguration {
 		sessionFactory.setDataSource(dataSource());
 		sessionFactory.setPackagesToScan(new String[]{"ua.kiev.inspector.repository.entity"});
 		Properties properties = new Properties();
-        properties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+        properties.put("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
         properties.put("hibernate.hbm2ddl.auto", "update");
+        properties.put("hibernate.default_shema", "public");
+        properties.put("hibernate.c3p0.timeout", 300);
         properties.put("hibernate.transaction.factory_class", "org.hibernate.transaction.JDBCTransactionFactory");
         properties.put("hibernate.enable_lazy_load_no_trans", true);
 		sessionFactory.setHibernateProperties(properties);
