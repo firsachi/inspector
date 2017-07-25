d66ea97fcca3d15436be0e984c6bd4148fc49437
firsachi <firsachi@localhost>
2017-07-26 01:16:54 +0300

    i simplify implementation

--- inspector/src/main/java/ua/kiev/inspector/controllers/HomeController.java
+++ inspector/src/main/java/ua/kiev/inspector/controllers/HomeController.java
@@ -8,42 +8,31 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
-import ua.kiev.inspector.model.TaskModel;
 import ua.kiev.inspector.model.UserModel;
-import ua.kiev.inspector.services.BaseService;
-import ua.kiev.inspector.services.HomeService;
-import ua.kiev.inspector.services.TaskService;
+import ua.kiev.inspector.repository.dao.RinspDocDao;
 
 @Controller
 public class HomeController {
 	
 	@Autowired
-	private HomeService homeService;
-	
-	@Autowired
-	private TaskService taskService;
+	private RinspDocDao rispDaoImpl;
 	
 	@RequestMapping(value = "/home", method = RequestMethod.GET)
-	public ModelAndView homePage(){
-		ModelAndView model = new ModelAndView();
-		model.setViewName("home");
+	public String homePage(Model model){
 		UserModel user = (UserModel)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 		// taskService.setUserEmail(user.getUserId());
-		model.addObject("user", user);
-		model.addObject("tasks", homeService.getAll(user.getUserId()));
-		return model;
+		model.addAttribute("user", user);
+		model.addAttribute("tasks", rispDaoImpl.getAllRisoDoc(user.getUserId()));
+		return "home";
 	}
 	
 	@GetMapping(value = "/home{taskId}" )
 	public String taskIdPage(@PathVariable("taskId") int id, Model model){
 		model.addAttribute("title", "task.title");
 		UserModel user = (UserModel)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-		taskService.setUserEmail(id);
 		model.addAttribute("user", user);
-		model.addAttribute("task", taskService.byId(id));
+		model.addAttribute("task", rispDaoImpl.getByRispDoc(id, user.getUserId()));
 		return "view-task";
 	}
-
 }
